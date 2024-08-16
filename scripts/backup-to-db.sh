fab11174a7f4574ebb9e1dcf176a97b97185ab10
George Pastushok <gpastushok@yahoo.com>
2024-08-16 18:06:43 +0000

    added db backup script and added a backup folder

---
+++ scripts/backup-to-db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# ----- THIS IS UNTESTED AND BASICALLY PSUEDOCODE -----
+#
+# This script should be made executable with chmod +x
+# and then called in the root? crontab.
+#
+# I could set a vars file that is imported here, maybe from the .env
+# import .env or something
+#
+# set db name
+DB="orders-dev-testing"
+# set proj dir
+PROJ="postgresql-node"
+# set filename:
+BAKNAME="$DB-backup-$(date +%F).sql"
+DEST="~/$PROJ/backup/"
+# Test if dir exists
+if [ ! -d $DEST ]; then
+	# make dir
+	mkdir $DEST
+fi
+# I could log the status of this script to a log file
+# echo "[ $(date) ] : backing up file $BAKNAME to $DEST"
+pg_dump -U postgres $DB > "$DEST$BAKNAME"
